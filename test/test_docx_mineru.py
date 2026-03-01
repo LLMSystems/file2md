97dde7f54fcb9c3feb6b7123d2005f2f1ca909bf
max <milk333445@gmail.com>
2026-03-01 15:16:50 +0800

    feat: 新增 PPTXConverter 類別以支持 PPTX 文件轉換，包含多個提供者管理及錯誤處理

--- test/test_docx_mineru.py
+++ test/test_docx_mineru.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 from src.core.types import ProcessOptions
 from src.providers.docx.mineru.docx_provider import DocxMinerUProvider
+from src.providers.docx.mammoth.docx_provider import DOCXMammothProvider
 from src.converters.docx.docx_converter import DOCXConverter
 
 if __name__ == "__main__":
     # 初始化
-    provider = DocxMinerUProvider(base_url="http://10.204.245.170:8962/")
+    provider1 = DocxMinerUProvider(base_url="http://localhost:8962/")
+    provider2 = DOCXMammothProvider()
     
+    converter = DOCXConverter(providers=[provider1, provider2], prefer="mammoth")
     # 測試檔案
     docx_files = ["./docs/test.docx"]
     
 
     
     # 執行轉換
-    result = provider.convert_files(
-        file_paths=docx_files,
+    result = converter.convert_files(
+        input_paths=docx_files,
         output_root=Path("./test_outputs/docx")
     )
     
-    print(result)
+    # print(result)
--- test/test_pdf.py
+++ test/test_pdf.py
@@ -3,15 +3,14 @@ from src.providers.pdf.mineru.pdf_provider import PDFMinerUProvider
 from src.converters.pdf.pdf_converter import PDFConverter
 
 if __name__ == "__main__":
-    provider = PDFMinerUProvider(base_url="http://10.204.245.170:8962/", output_root="./test_outputs")
+    provider = PDFMinerUProvider(base_url="http://10.204.245.170:8962/")
     pdfs = ["./pdfs/demo2.pdf"]
     converter = PDFConverter(providers=[provider], prefer="mineru")
     options = ProcessOptions(
         extra={
-            'pdf_backend': 'pipeline',
-            'pdf_parse_method': 'auto',
-            'pdf_keep_unzipped': True,
-            "pdf_provider": "mineru",   
+            'backend': 'pipeline',
+            'parse_method': 'auto',
+            'keep_unzipped': True,
             "draw_layout_bbox": True,
             "draw_span_bbox": True,    
             "return_images": True,
@@ -21,5 +20,5 @@ if __name__ == "__main__":
         }
     )
 
-    result = converter.convert_files(pdfs, output_root="./test_outputs", options=options)
+    result = converter.convert_files(pdfs, output_root="./test_outputs/pdf", options=options)
     print(result)
